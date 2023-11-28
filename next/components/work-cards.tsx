be4da0ecea855ad7bfe9b23b873288caa2323b44
Arseniy Kapshtyk <kapshtykas@yandex.ru>
2023-11-28 17:15:37 +0200

    update work

---
+++ next/components/work-cards.tsx
@@ -0,0 +1,44 @@
+import Link from "next/link";
+import { DrupalTaxonomyTerm } from "next-drupal";
+import { useTranslation } from "react-i18next";
+
+import { Page as PageType } from "@/lib/zod/page";
+import { WorkCard } from "@/lib/zod/paragraph";
+
+import { MediaImage } from "./media--image";
+
+export function WorkCards({ allPages, tags }) {
+  console.log("allPages", allPages);
+  return (
+    <div>
+      {tags.map((tag: DrupalTaxonomyTerm) => (
+        <div className="grid grid-cols-3 gap-6" key={tag.id}>
+          {allPages
+            .map((workPage: PageType) => (
+              <div key={workPage.id}>
+                {workPage.field_content_elements &&
+                  workPage.field_content_elements
+                    .filter(
+                      (element) => element.type === "paragraph--work_card",
+                    )
+                    .map((paragraph: WorkCard) => (
+                      <div key={paragraph.id}>
+                        <Link href={workPage.path.alias}>
+                          <MediaImage
+                            className="rounded-lg h-60 hover:saturate-150"
+                            media={paragraph.field_image}
+                          />
+                          <h3 className="text-primary-600 py-3 font-bold hover:underline">
+                            {workPage.title}
+                          </h3>
+                        </Link>
+                        <h1>{paragraph.field_excerpt}</h1>
+                      </div>
+                    ))}
+              </div>
+            ))}
+        </div>
+      ))}
+    </div>
+  );
+}
---
+++ next/components/workArticleCard.tsx
@@ -0,0 +1,44 @@
+import Image from "next/image";
+import Link from "next/link";
+
+import { absoluteUrl } from "@/lib/drupal/absolute-url";
+
+export function WorkArticleCard({ workArticle }) {
+  return (
+    <div
+      key={workArticle.id}
+      className="block max-w-[22rem] rounded-lg bg-white shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] dark:bg-neutral-700"
+    >
+      <Link href={workArticle.path.alias}>
+        <div className="relative overflow-hidden bg-cover bg-no-repeat h-64">
+          <Image
+            className="hover:saturate-150"
+            width={400}
+            height={500}
+            src={absoluteUrl(workArticle.field_image.uri.url)}
+            alt={workArticle.field_image.resourceIdObjMeta.alt}
+          />
+        </div>
+      </Link>
+
+      <div className="p-6">
+        <p className="text-base text-sm text-secondary-900">
+          <Link href="/all-articles">
+            {" "}
+            <span className="hover:underline">
+              {workArticle.type.split("--")[1].toUpperCase()}
+            </span>{" "}
+          </Link>
+        </p>
+      </div>
+
+      <Link href={workArticle.path.alias}>
+        <div className="p-6">
+          <h1 className="text-primary-600 font-bold hover:underline">
+            {workArticle.title}
+          </h1>
+        </div>
+      </Link>
+    </div>
+  );
+}
