af1ae71174139074c2e2eeba5075a9487fd6624a
Arseniy Kapshtyk <kapshtykas@yandex.ru>
2023-11-28 17:13:05 +0200

    update work

--- next/pages/work/[...slug].tsx
+++ next/pages/work/[...slug].tsx
@@ -1,121 +1,217 @@
-import { GetStaticPaths, GetStaticProps, InferGetStaticPropsType } from 'next'
-import { DrupalNode, DrupalTranslatedPath } from 'next-drupal'
-import { useTranslation } from 'next-i18next'
+import { GetStaticPaths, GetStaticProps, InferGetStaticPropsType } from "next";
+import {
+  DrupalNode,
+  DrupalTaxonomyTerm,
+  DrupalTranslatedPath,
+} from "next-drupal";
+import { useTranslation } from "next-i18next";
 
-import { Breadcrumbs } from '@/components/breadcrumbs'
-import { Meta } from '@/components/meta'
-import { Paragraph } from '@/components/paragraph'
+import { Breadcrumbs } from "@/components/breadcrumbs";
+import { Meta } from "@/components/meta";
+import { Paragraph } from "@/components/paragraph";
+import { WorkArticleCard } from "@/components/workArticleCard";
+import { WorkWorkCard } from "@/components/workWorkCard";
 import {
   createLanguageLinks,
-  LanguageLinks
-} from '@/lib/contexts/language-links-context'
-import { drupal } from '@/lib/drupal/drupal-client'
+  LanguageLinks,
+} from "@/lib/contexts/language-links-context";
+import { drupal } from "@/lib/drupal/drupal-client";
 import {
   getNodePageJsonApiParams,
-  ResourceType
-} from '@/lib/drupal/get-node-page-json-api-params'
-import { getNodeTranslatedVersions } from '@/lib/drupal/get-node-translated-versions'
-import { getCommonPageProps } from '@/lib/get-common-page-props'
-import { Page, validateAndCleanupPage } from '@/lib/zod/page'
+  ResourceType,
+} from "@/lib/drupal/get-node-page-json-api-params";
+import { getNodeTranslatedVersions } from "@/lib/drupal/get-node-translated-versions";
+import { getCommonPageProps } from "@/lib/get-common-page-props";
+import { Article, validateAndCleanupArticle } from "@/lib/zod/article";
+import { Page as PageType, validateAndCleanupPage } from "@/lib/zod/page";
 
-import NotFoundPage from '../404'
+import NotFoundPage from "../404";
 
 interface WorkPageProps {
-  workPage: Page
+  currentWorkPage: PageType;
+  allPages: PageType[];
+  allArticles: Article[];
+  tags: DrupalTaxonomyTerm[];
 }
 
 export default function WorkPage({
-  workPage
+  currentWorkPage,
+  allPages,
+  allArticles,
+  tags,
 }: InferGetStaticPropsType<typeof getStaticProps>) {
+  const { t } = useTranslation();
   const breadcrumbs = [
     {
-      title: 'Home',
-      url: '/'
+      title: t("homepage-link"),
+      url: "/",
     },
     {
-      title: 'Work',
-      url: '/work'
+      title: t("work-link"),
+      url: "/work",
     },
     {
-      title: workPage.title,
-      url: '/work/' + workPage.title
-    }
-  ]
-  console.log('work', workPage)
+      title: currentWorkPage.title,
+      url: "/work" + currentWorkPage.title,
+    },
+  ];
 
-  return workPage &&
-    workPage.field_page_type &&
-    workPage.field_page_type.name === 'Work' ? (
+  const allowedWorkPageTitles = [
+    "Tikkurila",
+    "Luke.fi",
+    "Fortum",
+    "HUS - Helsinki University Hospital",
+  ].filter(Boolean);
+
+  return tags.map(
+    (tag) =>
+      currentWorkPage &&
+      currentWorkPage.field_page_types &&
+      currentWorkPage.field_page_types.name === tag.name,
+  ) ? (
     <>
-      <Meta title={workPage.title} metatags={workPage.metatag} />
+      <Meta title={currentWorkPage.title} metatags={currentWorkPage.metatag} />
       <div className="container">
         {breadcrumbs?.length ? <Breadcrumbs items={breadcrumbs} /> : null}
       </div>
 
       <div>
-        {workPage.field_content_elements.map((paragraph) => (
+        {currentWorkPage.field_content_elements.map((paragraph) => (
           <Paragraph key={paragraph.id} paragraph={paragraph} />
         ))}
       </div>
+
+      {allowedWorkPageTitles.includes(currentWorkPage.title) ? (
+        <div className="mt-20">
+          <h1 className="font-bold my-4">RELATED CONTENT</h1>
+          {tags.map((tag) => (
+            <div className="flex space-x-6">
+              {allPages
+                .filter(
+                  (workPages) => workPages.field_page_types?.name === tag.name,
+                )
+                .filter(
+                  (workPages) => workPages.title !== currentWorkPage.title,
+                )
+                .slice(0, 4)
+                .map((workPage) => (
+                  <WorkWorkCard workPage={workPage} />
+                ))}
+            </div>
+          ))}
+        </div>
+      ) : null}
+
+      {currentWorkPage.title === "Trimble" ? (
+        <div className="mt-20">
+          <h1 className="font-bold mb-4">RELATED CONTENT</h1>
+          <div className="grid grid-cols-3 gap-3">
+            {allArticles
+              .filter((workArticles) =>
+                workArticles.field_tags.some(
+                  (field_tag) => field_tag?.name === "Drupal",
+                ),
+              )
+              .sort(
+                (a, b) =>
+                  new Date(b.created).getTime() - new Date(a.created).getTime(),
+              )
+              .slice(0, 3)
+              .map((workArticle) => (
+                <WorkArticleCard workArticle={workArticle} />
+              ))}
+          </div>
+        </div>
+      ) : null}
     </>
   ) : (
     <NotFoundPage />
-  )
+  );
 }
 
 export const getStaticPaths: GetStaticPaths = async (context) => {
-  const paths = await drupal.getStaticPathsFromContext('node--page', context)
+  const paths = await drupal.getStaticPathsFromContext("node--page", context);
   return {
     paths: paths,
-    fallback: 'blocking'
-  }
-}
+    fallback: "blocking",
+  };
+};
 
 export const getStaticProps: GetStaticProps<WorkPageProps> = async (
-  context
+  context,
 ) => {
-  const path: DrupalTranslatedPath =
-    await drupal.translatePathFromContext(context)
+  const path: DrupalTranslatedPath = await drupal.translatePathFromContext(
+    context,
+    { pathPrefix: "/work" },
+  );
+
+  console.log("path:", path)
 
   if (!path) {
     return {
-      notFound: true
-    }
+      notFound: true,
+    };
   }
 
-  const type = path.jsonapi.resourceName as ResourceType
+  const type = path.jsonapi.resourceName as ResourceType;
 
   const resource = await drupal.getResourceFromContext<DrupalNode>(
     path,
     context,
     {
-      params: getNodePageJsonApiParams(type).getQueryObject()
-    }
-  )
+      params: getNodePageJsonApiParams(type).getQueryObject(),
+    },
+  );
+
+  /*   console.log("resource:", resource);
+   */
+  const nodeTranslations = await getNodeTranslatedVersions(
+    resource,
+    context,
+    drupal,
+  );
+
+  const languageLinks = createLanguageLinks(nodeTranslations);
 
   if (!resource) {
-    throw new Error(`Failed to fetch resource: ${path.jsonapi.individual}`)
+    throw new Error(`Failed to fetch resource: ${path.jsonapi.individual}`);
   }
 
   if (!context.preview && resource?.status === false) {
     return {
-      notFound: true
-    }
+      notFound: true,
+    };
   }
-  /*  const nodeTranslations = await getNodeTranslatedVersions(
-       resource,
-       context,
-       drupal,
-     );
-   
-     const languageLinks = createLanguageLinks(nodeTranslations); */
+
+  const pages = await drupal.getResourceCollectionFromContext<DrupalNode[]>(
+    "node--page",
+    context,
+    {
+      params: getNodePageJsonApiParams("node--page").getQueryObject(),
+    },
+  );
+
+  const articles = await drupal.getResourceCollectionFromContext<DrupalNode[]>(
+    "node--article",
+    context,
+    {
+      params: getNodePageJsonApiParams("node--article").getQueryObject(),
+    },
+  );
+
+  const tags = await drupal.getResourceCollectionFromContext<
+    DrupalTaxonomyTerm[]
+  >("taxonomy_term--page_types", context, {});
 
   return {
     props: {
       ...(await getCommonPageProps(context)),
-      workPage: validateAndCleanupPage(resource)
-      /* languageLinks, */
+      currentWorkPage: validateAndCleanupPage(resource),
+      allPages: pages.map((node) => validateAndCleanupPage(node)),
+      allArticles: articles.map((node) => validateAndCleanupArticle(node)),
+      languageLinks,
+      tags,
     },
-    revalidate: 60
-  }
-}
+    revalidate: 60,
+  };
+};
