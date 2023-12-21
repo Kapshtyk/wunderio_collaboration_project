136f0cc65241adac6f0ff58d26a2989fb1a0237f
shree0007 <shree.bhusal07@gmail.com>
2023-12-21 10:52:49 +0200

    latest styling

--- next/components/article.tsx
+++ next/components/article.tsx
@@ -40,30 +40,7 @@ export function Article({ article, ...props }: ArticleProps) {
       {article.field_excerpt && (
         <div className="my-4 text-xl">{article.field_excerpt}</div>
       )}
-      {/* <div className="mb-4 bg-primary-50 p-2 md:w-3/12 py-4 rounded-lg">
-        <div className="flex items-center ">
 
-          <div className="w-[350px] m-auto">
-            {article.uid?.display_name && (
-              <span className="text-accent-hugs text-md">
-                {t("posted-by", { author: article.uid?.display_name })} -{" "}
-              </span>
-            )}
-            <span className="text-accent-hugs text-md">{formatDate(article.created, router.locale)}</span>
-          </div>
-        </div>
-        {article.uid?.field_profile_picture?.uri && (
-          <div className="w-12 h-12 m-auto my-2">
-            <Image
-              src={absoluteUrl(article.uid?.field_profile_picture?.uri.url)}
-              width={100}
-              height={100}
-              className="rounded-full object-cover h-full w-full mr-3"
-              alt={article.uid?.field_profile_picture.resourceIdObjMeta.alt}
-            />
-          </div>
-        )}
-      </div> */}
       <div className="flex items-center justify-center bg-primary-50 p-2 rounded-md shadow-md mb-4">
         {article.uid?.field_profile_picture?.uri && (
           <div className="w-12 h-12">
